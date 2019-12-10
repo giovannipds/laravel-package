65cc585bea7f33b683e6bf54546d30925ba46f1e
Giovanni Pires da Silva <giovanni@somosgad.com>
2019-12-10 11:39:45 -0300

    Add the service provider registering methods

--- packages/wisdmlabs/todolist/src/TodolistServiceProvider.php
+++ packages/wisdmlabs/todolist/src/TodolistServiceProvider.php
@@ -13,7 +13,7 @@ class TodolistServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->make('wisdmLabs\todolist\TodolistController');
     }
 
     /**
@@ -23,6 +23,11 @@ class TodolistServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $this->loadRoutesFrom(__DIR__.'/routes.php');
+        $this->loadMigrationsFrom(__DIR__.'/migrations');
+        $this->loadViewsFrom(__DIR__.'/views', 'todolist');
+        $this->publishes([
+            __DIR__.'/views' => base_path('resources/views/wisdmlabs/todolist'),
+        ]);
     }
 }
